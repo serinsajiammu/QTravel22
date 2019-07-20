417298c30436fea711f77da68b46fb721bdfe613
Serin Saji <serinsajiammu@gmail.com>
2019-07-20 09:22:12 +0530

    nil

---
+++ app/src/main/java/com/example/qtravel/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.qtravel;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+
+// Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 5*1000);
+    }
+}
